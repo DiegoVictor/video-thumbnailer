2bc4195ef0af37a70ab4a433b5bd3f7a8533b4e7
Diego Victor <diegovictorgonzaga@gmail.com>
2022-08-23 00:26:09 -0300

    Add serverless environment variables

--- serverless.ts
+++ serverless.ts
@@ -11,6 +11,10 @@ const serverlessConfiguration: AWS = {
       minimumCompressionSize: 1024,
       shouldStartNameWithService: true,
     },
+    environment: {
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+      NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
+    },
   },
   functions: {
   package: { individually: true },
